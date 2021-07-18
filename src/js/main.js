ad212d04f668485b494103578c9a1eed5eadcd12
whalerice <54788767+whalerice@users.noreply.github.com>
2021-07-18 20:23:24 +0900

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,6 @@
-// 'use strict';
+import '../vendor/lib';
+import '../vendor/reset.css';
+import '../scss/main.scss';
+// const css = require('../css/main.css');
 
-import '@babel/polyfill';
-import { pi, power, Foo } from './lib';
-
-console.log(pi);
-console.log(power(pi, pi));
-
-const f = new Foo();
-console.log(f.foo());
-console.log(f.bar());
-
-// var _lib = require('./lib');
-
-// console.log(_lib.pi);
-// console.log((0, _lib.power)(_lib.pi, _lib.pi));
-
-// const f = _lib.Foo();
-// console.log(f.foo());
-// console.log(f.bar());
+console.log('webpack!!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,60 @@
 const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-	entry: ['./src/js/main.js'],
+	entry: './src/js/main.js',
 	output: {
-		path: path.resolve(__dirname, 'dist/js'),
-		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		// filename: './js/bundle.[contenthash].js',
+		filename: './js/bundle.min.js',
 		clean: true,
 	},
 	module: {
 		rules: [
+			// test: /\.(jpe?g|png|gif|svg)$/i,
+			// name: 'images/[name].[ext]?[hash]',
 			{
-				test: /\.js$/,
-				include: [path.resolve(__dirname, 'src/js')],
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-						plugins: ['@babel/plugin-proposal-class-properties'],
-					},
-				},
+				test: /\.(scss|css)$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
 			},
+			// {
+			// 	test: /\.(png|jpe?g|gif|svg)$/i,
+			// 	loader: 'file-loader',
+			// 	options: {
+			// 		name: './images/[name].[ext]?[hash]',
+			// 	},
+			// },
+			// {
+			// 	test: /\.(png|svg|jpg|gif)$/,
+			// 	loader: 'url-loader',
+			// 	options: {
+			// 		limit: 10000,
+			// 		name: 'images/[name].[hash:7].[ext]',
+			// 	},
+			// },
 		],
 	},
-	// devtool: 'source-map',
-	mode: 'development',
-	// mode: 'production',
+	plugins: [
+		new HtmlPlugin({
+			minify: {
+				collapseWhitespace: true,
+			},
+			hash: true,
+			template: './index.html',
+		}),
+		new CopyWebpackPlugin({
+			patterns: [{ from: 'src/images', to: 'images' }],
+		}),
+		new MiniCssExtractPlugin({
+			// filename: './css/[name].[contenthash].css',
+			// chunkFilename: './css/[id].[contenthash].css',
+			filename: './css/[name].min.css',
+			chunkFilename: './css/[id].min.css',
+		}),
+	],
+	devServer: {
+		host: 'localhost',
+	},
 };
